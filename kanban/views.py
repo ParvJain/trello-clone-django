ba50394d8ac648c735e1755e70c035fb4b67698d
Parv Jain <parv.wutizup@gmail.com>
2017-08-12 10:27:15 +0530

    Basic User account scaffolding

---
+++ kanban/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+# this login required decorator is to not allow to any
+# view without authenticating
+@login_required(login_url="login/")
+def home(request):
+    return render(request,"home.html")
+
+def create_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("User created successfully!")
+    else:
+        form = UserCreationForm()
+        return render(request, 'auth/create_user.html', {'form': form})
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
--- trello_forge/urls.py
+++ trello_forge/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+
+from django.contrib.auth import views as sign_in_view
+from kanban import views
+from kanban.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('kanban.urls')),
+    url(r'^login/$', sign_in_view.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', sign_in_view.logout, {'next_page': '/login'}),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
