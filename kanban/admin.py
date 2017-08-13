d443168ee86649c6413536cfe89ef21d992a0f90
Parv Jain <parv.wutizup@gmail.com>
2017-08-13 15:20:06 +0530

    Models and UI proto

--- kanban/admin.py
+++ kanban/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Task
+
+admin.site.register(Category)
+admin.site.register(Task)
--- kanban/models.py
+++ kanban/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    is_visible = models.BooleanField(default=True)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return '{} - {} - {} - {}'.format(self.id, self.title, self.is_visible,
+                                        self.user.id)
+
+    class Meta:
+        default_related_name = "category"
+        ordering = ['-id']
+
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    is_visible = models.BooleanField(default=True)
+    weight = models.FloatField(unique=True)
+    category = models.ForeignKey(Category)
+
+
+    def __str__(self):
+        return '{} - {} - {} - {} - {} - {}'.format(
+                                                self.id, self.title,
+                                                self.description,
+                                                self.is_visible, self.weight,
+                                                self.category.id)
+
+    class Meta:
+        default_related_name = "tasks"
+        ordering = ['-weight']
