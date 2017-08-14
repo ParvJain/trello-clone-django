0f8e4e9c7ec685bda4fee9cdd6c670f4576e8ac7
Parv Jain <parv.wutizup@gmail.com>
2017-08-14 09:33:42 +0530

    Angular cleanup and structuring

---
+++ static/js/controller.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.controller', [])
+    .controller('Kanban', Kanban);
+
+  Kanban.$inject = ['$scope'];
+
+  function Kanban($scope) {
+    
+    $scope.groups = [
+      {
+        name: 'Group A',
+        items: [{name: 'Item A'},{name: 'Item B'},{name: 'Item C'},{name: 'Item D'}]
+      },
+      {
+        name: 'Group B',
+        items: [{name: 'Item 1'},{name: 'Item 2'},{name: 'Item 3'},{name: 'Item 4'}]
+      }
+    ];
+
+
+    $scope.$on('nested-bag.drop', function(e, el, container, source, sibling) {
+      console.log(sibling.scope());
+      console.log(container.scope());
+    });
+
+  }
+})();
